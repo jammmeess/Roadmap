f0ea56c921af154fc0c3bab60f3b26093fb7d513
jammmeess <james_bomediano@hotmail.com>
2024-03-19 15:20:55 +0800

    Updated Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
 import  React from "react";
 
+import Navbar from "./Navbar";
+import Welcome from "./Welcome";
+
 const Home = () => {
     return (
         <section>
-
+            <div>
+                <Navbar />
+            </div>
+            <main>
+                <Welcome/>
+            </main>
         </section>
     );
 };
