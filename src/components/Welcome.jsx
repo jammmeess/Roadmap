c1363a2504a00b738f383c167d01abf33a1aed4e
jammmeess <james_bomediano@hotmail.com>
2024-03-19 15:22:22 +0800

    Added a hero banner on my welome page

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import HeroBanner from "../img/herobanner.jpg";
+
+const Welcome = () => {
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <div className="hero-image col-lg-12 d-flex justify-content-end">
+          <img src={HeroBanner} />
+        </div>
+
+        <div className="hero-text text-start overlay">
+          <h1 className="fw-bolder display-1 hero-title">Hey There,</h1>
+          <h1 className="fw-bolder display-1 hero-title mb-5 higlight">
+            Explore Accenture PH with me!
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Welcome;
