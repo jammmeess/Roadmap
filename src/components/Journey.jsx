1b7875f4edaf7b694a330d093c469c049856ffad
jammmeess <james_bomediano@hotmail.com>
2024-03-19 18:13:44 +0800

    Added The Journey Section

---
+++ src/components/Journey.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import JourneyOne from "../img/roadmap/journey-begins.jpg";
+import JourneyTwo from "../img/roadmap/journey-begins2.jpg";
+
+const Journey = () => {
+  return (
+    <section className="journey-section" id="Journey">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>My Journey Begins</h1>
+          </div>
+          <div className="col-lg-5">
+            <img src={JourneyOne} className="journeyOne" />
+          </div>
+          <div className="col-lg-5 text-light journeyOne-caption">
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              sapiente maxime distinctio asperiores iusto harum voluptates,
+              magnam odit laboriosam commodi natus ex, deleniti explicabo
+              exercitationem?
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+              vero iure, odio porro expedita debitis dolorum distinctio. Cumque
+              possimus ut ipsam aut dolorum, nemo optio.
+            </p>
+          </div>
+          <div className="col-lg-5 text-light journeyOne-caption ">
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              sapiente maxime distinctio asperiores iusto harum voluptates,
+              magnam odit laboriosam commodi natus ex, deleniti explicabo
+              exercitationem?
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+              vero iure, odio porro expedita debitis dolorum distinctio. Cumque
+              possimus ut ipsam aut dolorum, nemo optio.
+            </p>
+          </div>
+          <div className="col-lg-5 ps-5">
+            <img src={JourneyTwo} className="journeyTwo" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Journey;
