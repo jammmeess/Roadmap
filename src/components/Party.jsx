a81e434082ddfc5d81581df9b5a99078ca0be3c1
jammmeess <james_bomediano@hotmail.com>
2024-03-19 22:25:01 +0800

    Added the Party Section

---
+++ src/components/Party.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import PartyOne from "../img/roadmap/party1.jpg";
+import PartyTwo from "../img/roadmap/party2.JPG";
+
+const Party = () => {
+    return (
+      <section className="party-section" id="Party">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h3 className="display-6">
+                Let's get this <span id="accenture">Party</span> Started!
+              </h3>
+            </div>
+            <div className="col-lg-6">
+              <img src={PartyOne} className="party-one"></img>
+            </div>
+            <div className="col-lg-5 partyone-caption">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                eos ipsum corporis. Quis, architecto! Maiores temporibus fugit
+                quaerat illo ratione debitis, voluptatibus, repellendus
+                reprehenderit minus corrupti totam itaque.
+              </p>
+            </div>
+            <div className="col-lg-5 partytwo-caption me-5">
+                <h4 className="mb-3">Meet my <span id="accenture">P.E.P.</span> Team</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                eos ipsum corporis. Quis, architecto! Maiores temporibus fugit
+                quaerat illo ratione debitis, voluptatibus, repellendus
+                reprehenderit minus corrupti totam itaque.
+              </p>
+            </div>
+            <div className="col-lg-6">
+              <img src={PartyTwo} className="party-one"></img>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+}
+export default Party;
