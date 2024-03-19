c57bde17b4322b5067ea000bb99e6df89639125b
jammmeess <james_bomediano@hotmail.com>
2024-03-19 19:27:35 +0800

    Added the Accenture Section

---
+++ src/components/Accenture.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import AccenturePhoto from "../img/roadmap/accenturephilippines.jpg";
+
+const Accenture = () => {
+  return (
+    <section className="accenture-section" id="Accenture">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 ">
+            <img src={AccenturePhoto} className="accenture-photo" />
+          </div>
+          <div className="col-lg-12  accenture-caption">
+            <h3 className="display-5">My First Day in <span id="accenture">Accenture</span></h3>
+            <p className="pt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              laboriosam esse placeat temporibus cumque voluptatem ex ratione
+              cupiditate ea corrupti, veritatis repellat omnis quia sed officiis
+              culpa facilis voluptatum, praesentium quibusdam eveniet facere
+              obcaecati aliquam. Exercitationem, obcaecati, modi explicabo
+              dolorum quas aliquam aperiam, delectus at quis fugiat officiis sed
+              sequi.
+            </p>
+            <p className="pt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              laboriosam esse placeat temporibus cumque voluptatem ex ratione
+              cupiditate ea corrupti, veritatis repellat omnis quia sed officiis
+              culpa facilis voluptatum, praesentium quibusdam eveniet facere
+              obcaecati aliquam. Exercitationem, obcaecati, modi explicabo
+              dolorum quas aliquam aperiam, delectus at quis fugiat officiis sed
+              sequi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Accenture;
