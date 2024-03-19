3d876a3454ae47f34f2668f296de2286b96086aa
jammmeess <james_bomediano@hotmail.com>
2024-03-19 22:53:47 +0800

    Added Continue Section

---
+++ src/components/Continue.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Continue = () =>{
+    return (
+      <section className="continue-section" id="Continue">
+        <div className="container">
+          <div className="Row">
+            <div className="col-lg-12 continue-quote">
+              <h2 className="display-3">
+                Two <span id="accenture">Roads </span>Diverged in a wood, and I
+                took the one <span id="accenture">less traveled </span>
+                by and that has made all the{" "}
+                <span id="accenture">difference</span>.
+              </h2>
+              <p>- Robert Frost</p>
+            </div>
+            <div className="col-lg-12 continue-quote">
+              <h2 className="display-3">
+                For more <span id="accenture">A</span>dventures to come!
+              </h2>
+              <h6 className="display-6 mt-5">Thank you my <span id="accenture">Accenture </span>Family!</h6>
+              <h6 className="display-6">- <i>Marian Rae Royandoyan</i></h6>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default Continue;
