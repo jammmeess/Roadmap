9cfeee7c0974f2897b964a38f1d498e1a911f116
jammmeess <james_bomediano@hotmail.com>
2024-03-19 15:21:50 +0800

    Added a Side Navigation Bar for immersion

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+
+const Navbar = () => {
+  useEffect(() => {
+    const links = document.querySelectorAll(".nav a");
+
+    links.forEach((link) => {
+      if (link.href === window.location.href) {
+        link.classList.add("active");
+        link.parentElement.classList.add("active");
+      }
+    });
+  }, []);
+
+  return (
+    <section>
+      <aside className="sidebar">
+        <nav className="nav ps-2">
+          <ul>
+            <li>
+              <a href="#">Welcome to my Roadmap</a>
+            </li>
+            <li>
+              <a href="#">About Me</a>
+            </li>
+            <li>
+              <a href="#">My Journey Begins</a>
+            </li>
+            <li>
+              <a href="#">Accenture Philippines</a>
+            </li>
+            <li>
+              <a href="#">My Second Home</a>
+            </li>
+            <li>
+              <a href="#">Meet my Team</a>
+            </li>
+            <li>
+              <a href="#">Let's Get this Party Started</a>
+            </li>
+            <li>
+              <a href="#">My Journey Continues</a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </section>
+  );
+};
+
+export default Navbar;
