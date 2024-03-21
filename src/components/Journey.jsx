ac57a5d14a6fd333cbf79927ef264b1319cca08d
James Bomediano <james_bomediano@hotmail.com>
2024-03-21 11:00:31 +0800

    Spacing Changes

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -6,12 +6,14 @@ const Journey = () => {
   return (
     <section className="journey-section" id="Journey">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-12 ms-5 mt-5">
-            <h1>My Journey Begins</h1>
+        <div className="row px-5 mx-5">
+          <div className="col-lg-12 ms-5 my-5 ">
+            <h1>
+              <span id="accenture-dark">My Journey</span> Begins
+            </h1>
           </div>
-          <div className="col-lg-5 ms-5">
-            <img src={JourneyOne} className="journeyOne" />
+          <div className="col-lg-5">
+            <img src={JourneyOne} className="journey-one" />
           </div>
           <div className="col-lg-5 mt-5 pt-5 text-light journeyOne-caption">
             <h3>Lorem ipsum dolor sit amet.</h3>
@@ -27,7 +29,7 @@ const Journey = () => {
               possimus ut ipsam aut dolorum, nemo optio.
             </p>
           </div>
-          <div className="col-lg-5 text-light ms-5 ps-5 journeyOne-caption ">
+          <div className="col-lg-5 text-light ps-3 me-5 journeyOne-caption ">
             <h3>Lorem ipsum dolor sit amet.</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
@@ -41,7 +43,7 @@ const Journey = () => {
               possimus ut ipsam aut dolorum, nemo optio.
             </p>
           </div>
-          <div className="col-lg-5 ms-5 ps-5">
+          <div className="col-lg-5">
             <img src={JourneyTwo} className="journeyTwo" />
           </div>
         </div>
