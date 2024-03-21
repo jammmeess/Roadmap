63990fffef282ea84d9c287696692b597ed59dff
James Bomediano <james_bomediano@hotmail.com>
2024-03-21 11:02:21 +0800

    Minor Spacing Changes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -12,7 +12,7 @@ const Welcome = () => {
         <div className="hero-text text-start overlay">
           <h1 className="fw-bolder display-1 hero-title">Hey There,</h1>
           <h1 className="fw-bolder display-1 hero-title mb-5 higlight">
-            Explore <span id="accenture">Accenture PH</span> with me!
+            Explore <span id="accenture-dark">Accenture PH</span> with me!
           </h1>
         </div>
       </div>
