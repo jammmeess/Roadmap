517220913694f22aeac767fb13687843c55084c9
James Bomediano <james_bomediano@hotmail.com>
2024-03-21 10:59:07 +0800

    Changed the Font Color

--- src/components/Continue.jsx
+++ src/components/Continue.jsx
@@ -7,19 +7,19 @@ const Continue = () => {
         <div className="Row">
           <div className="col-lg-12 continue-quote px-5">
             <h2 className="display-3">
-              Two <span id="accenture">Roads </span>Diverged in a wood, and I
-              took the one <span id="accenture">less traveled </span>
+              Two <span id="accenture-dark">Roads </span>Diverged in a wood, and
+              I took the one <span id="accenture-dark">less traveled </span>
               by and that has made all the{" "}
-              <span id="accenture">difference</span>.
+              <span id="accenture-dark">difference</span>.
             </h2>
             <p>- Robert Frost</p>
           </div>
           <div className="col-lg-12 continue-quote px-5">
             <h2 className="display-3">
-              For more <span id="accenture">A</span>dventures to come!
+              For more <span id="accenture-dark">A</span>dventures to come!
             </h2>
             <h6 className="display-6 mt-5">
-              Thank you my <span id="accenture">Accenture </span>Family!
+              Thank you my <span id="accenture-dark">Accenture </span>Family!
             </h6>
             <h6 className="display-6">
               - <i>Marian Rae Royandoyan</i>
